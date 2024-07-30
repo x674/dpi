339d46fdc174d822d78766fd12f30798a4db6dad
Roman Z <iron-nmen@ya.ru>
2024-07-31 09:42:18 +1000

    Add files via upload

---
+++ youtube-domain.cmd
@@ -0,0 +1,13 @@
+goodbyedpi.exe --blacklist youtube-domain.txt -5
+
+@ECHO OFF
+PUSHD "%~dp0"
+set _arch=x86
+IF "%PROCESSOR_ARCHITECTURE%"=="AMD64" (set _arch=x86_64)
+IF DEFINED PROCESSOR_ARCHITEW6432 (set _arch=x86_64)
+PUSHD "%_arch%"
+
+start "" goodbyedpi.exe --blacklist ..\youtube-domain.txt -6
+
+POPD
+POPD
